fb8e04964d1bfc38a0ba736898667d7c28c0be9a
omjagtap100 <om.jagtap@tribox.me>
2025-07-30 19:13:44 +0530

    KeyCloak Auth Implemented

---
+++ Controllers/Test.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace paytrack_api.Controllers
+{
+ 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        //[Authorize]
+        [HttpGet("{id}")]
+        public IActionResult ValidateToken()
+        {
+            var tokenHeader = Request.Headers["Authorization"].ToString();
+            Console.WriteLine(tokenHeader);
+            var userName = HttpContext.User.Identity?.Name ?? "Unknown";
+            var claims = HttpContext.User.Claims.Select(c => new { c.Type, c.Value });
+
+            return Ok(new
+            {
+                Message = "Token validated successfully!",
+                User = userName,
+                AuthorizationHeader = tokenHeader,
+                Claims = claims
+            });
+        }
+        [Authorize]
+        [HttpGet()]
+        public IActionResult ValidateTokenAuthorized()
+        {
+            var tokenHeader = Request.Headers["Authorization"].ToString();
+            Console.WriteLine(tokenHeader);
+            var userName = HttpContext.User.Identity?.Name ?? "Unknown";
+            var claims = HttpContext.User.Claims.Select(c => new { c.Type, c.Value });
+
+            return Ok(new
+            {
+                Message = "Token validated successfully!",
+                User = userName,
+                AuthorizationHeader = tokenHeader,
+                Claims = claims
+            });
+        }
+
+    }
+}
